0e7f3a79abad201f70b2355fd11b482d0774778b
ItalianJack <92689827+ItalianJack@users.noreply.github.com>
2024-01-25 11:59:59 -0500

    removed header & footer loading & cleaned up

--- scripts/loadHeaderFooter.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.get('../components/header.html', (data) => {
-        $('header').replaceWith(data);
-    });
-    $.get('../components/footer.html', (data) => {
-       $('footer').replaceWith(data);
-    });
-});
