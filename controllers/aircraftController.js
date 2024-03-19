4f1a4ae157c348c7197ea4b45ebe352300e04492
Jack Krueger <92689827+ItalianJack@users.noreply.github.com>
2024-03-19 19:20:33 -0400

    functional

--- controllers/aircraftController.js
+++ controllers/aircraftController.js
@@ -1,10 +1,12 @@
-const model = require('../models/aircraft');
+const Aircraft = require('../models/aircraft');
 
 // Index - GET /aircraft
 exports.index = (req, res) => {
     const search = req.query.search;
-    const aircraft = model.find(search);
-    res.render('aircraft/index', {aircraft, getConditionString: model.getConditionString});
+    Aircraft.find({"$or": [{title: new RegExp(search, 'i')}, {details: new RegExp(search, 'i')}]})
+        .then((aircraft) => {
+            res.render('aircraft/index', {aircraft});
+        });
 }
 
 // New - GET /aircraft/new
@@ -14,65 +16,84 @@ exports.new = (req, res) => {
 
 // Create - POST /aircraft
 exports.create = (req, res) => {
-    const newAircraft = req.body;
+    let newAircraft = new Aircraft(req.body);
     newAircraft.image = req.file.filename;
-    const id = model.create(newAircraft);
-    res.redirect(`/aircraft/${id}`);
+    newAircraft.save()
+        .then((savedAircraft) => {
+            console.log(savedAircraft);
+            res.redirect(`/aircraft/${savedAircraft._id}`);
+        })
+        .catch(err => {
+            console.log('MONGOOSE ERROR: ' + err);
+        })
 }
 
 // Show - GET /aircraft/:id
 exports.show = (req, res, next) => {
     const id = req.params.id
-    const aircraft = model.findById(id);
-    if (aircraft) {
-        res.render('aircraft/show', {aircraft, getConditionString: model.getConditionString});
-    } else {
-        let err = new Error(`No aircraft found with id ${id}`);
-        err.status = 404;
-        next(err);
-    }
+    Aircraft.findById(id)
+        .then((aircraft) => {
+            res.render('aircraft/show', {aircraft});
+        })
+        .catch((err) => {
+            printKeyValuePairs(err);
+            err = new Error(`No aircraft found with id ${id}`);
+            err.status = 404;
+            next(err);
+        });
 }
 
 // Edit - GET /aircraft/:id/edit
 exports.edit = (req, res, next) => {
     const id = req.params.id;
-    const aircraft = model.findById(id);
-    if (aircraft) {
-        res.render('aircraft/edit', {aircraft});
-    } else {
-        let err = new Error(`No aircraft found with id ${id}`);
-        err.status = 404;
-        next(err);
-    }
+    Aircraft.findById(id)
+        .then((aircraft) => {
+            res.render('aircraft/edit', {aircraft});
+        })
+        .catch((err) => {
+            console.log('MONGOOSE ERROR: ' + err);
+            err = new Error(`No aircraft found with id ${id}`);
+            err.status = 404;
+            next(err);
+        });
 }
 
 // Update - PUT /aircraft/:id
 exports.update = (req, res, next) => {
-    const updatedAircraft = req.body;
+    const id = req.params.id;
+    let updatedAircraft = req.body;
     if (req.file) {
         updatedAircraft.image = req.file.filename;
     }
-    const id = req.params.id;
-    const success = model.updateById(id, updatedAircraft);
-    if (success) {
-        res.redirect(`/aircraft/${id}`);
-    } else {
-        let err = new Error(`No aircraft found with id ${id}`);
-        err.status = 404;
-        next(err);
-    }
+    Aircraft.updateOne({_id: id}, updatedAircraft)
+        .then(() => {
+            res.redirect(`/aircraft/${id}`);
+        })
+        .catch((err) => {
+            console.log('MONGOOSE ERROR: ' + err);
+            err = new Error(`No aircraft found with id ${id}`);
+            err.status = 404;
+            next(err);
+        });
 }
 
 // Destroy - DELETE /aircraft/:id
 exports.destroy = (req, res, next) => {
     const id = req.params.id;
-    const success = model.deleteById(id);
-    if (success) {
-        res.redirect('/aircraft');
-    } else {
-        let err = new Error(`No aircraft found with id ${id}`);
-        err.status = 404;
-        next(err);
-    }
+    Aircraft.deleteOne({_id: id})
+        .then(() => {
+            res.redirect('/aircraft');
+        })
+        .catch((err) => {
+            console.log('MONGOOSE ERROR: ' + err);
+            err = new Error(`No aircraft found with id ${id}`);
+            err.status = 404;
+            next(err);
+        });
+}
 
+function printKeyValuePairs(err) {
+    Object.keys(err).forEach(key => {
+        console.log(key + ': ' + err[key]);
+    });
 }
